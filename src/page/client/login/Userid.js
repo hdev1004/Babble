5935ce8c79c82904ae72a887be61764066defaaf
DESKTOP-5SPU3RG\4stec <siyeon00@kakao.com>
2023-05-30 13:07:55 +0900

    FEAT : 아이디창/ 비밀번호창 구현중

--- src/page/client/login/Userid.js
+++ src/page/client/login/Userid.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LoginDiv } from './css/login';
-import { IdFoundForm, IdFoundTitle } from './css/foundId';
+import { IdFoundForm, IdFoundTitle, Idtab, TabSelect } from './css/foundId';
 
 const Userid = () => {
+    const [tabBorder, setTabBorder] = useState(0);
+    console.log(tabBorder)
     return (
         <LoginDiv>
             <IdFoundForm>
@@ -11,6 +13,15 @@ const Userid = () => {
                     <span className='title'>babble</span>
                 </IdFoundTitle>
 
+                <TabSelect >
+                    <Idtab onClick={()=>{setTabBorder(0)}} className={tabBorder === 0 ? "tab_border" : "none_border"}>
+                        <span>아이디 찾기</span>
+                    </Idtab>
+                    <Idtab onClick={()=>{setTabBorder(1)}} className={tabBorder === 1 ? "tab_border" : "none_border"}>
+                        <span>비밀번호 찾기</span>
+                    </Idtab>
+                </TabSelect>
+            
             </IdFoundForm>
         </LoginDiv>
 
