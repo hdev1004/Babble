a70f773f25fc9197699c8be1d80bf8ae3fb95ffb
jxxny <axcv__re@naver.com>
2023-06-22 00:34:49 +0900

    FEAT : BEST 게시판 게시글

--- src/page/client/Board/Main/css/BestPost.js
+++ src/page/client/Board/Main/css/BestPost.js
@@ -2,7 +2,7 @@ import { styled } from "styled-components"
 
 
 export const BestPostForm = styled.div`
-    padding: 15px;
+    padding: 20px 0px 10px 0px;
     width: 350px;
     height: 871px;
     background: #FFFFFF;
@@ -11,24 +11,98 @@ export const BestPostForm = styled.div`
 `
 
 export const BestPostTitle = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center; 
-
     & > div {
+        align-items: center;
+        height: calc(100% / 10);
         font-size: 18px;
-        height: 47px;
-        line-height: 47px;
     }
 `
 
 export const IconAndTitle = styled.div`
     display: flex;
     flex-direction: row;
+    padding-left: 100px;
 
     img {
         width: 29px;
         height: 29px;
-        padding-right: 10px;
+        margin-right: 20px;
+    }
+`
+
+
+export const BestNotice = styled.div`
+    border: 1px solid #0085FF;
+    border-radius: 30px;
+    margin : 20px 10px 25px 10px;
+    
+    .BNotice {
+        font-size : 13px;
+        text-align: center;
+        line-height: 15px;
+        padding : 8px 0px 8px 0px;
+        color : #0085FF;
+    }
+`
+
+export const BestPostList = styled.div`
+
+`
+
+
+export const LIkeAComent = styled.div`
+    font-size : 16px;
+    display: inline-block;
+    margin-right : 15px;
+    
+    .LikeBtn {
+        color : #FF0000;
+        display: inline-block;
+    }
+
+    .ComentCnt {
+        color : #0085FF;
+        display: inline-block;
+    }
+
+    .Like, .Coment {
+        margin : 0px 5px 0px 0px;
+    }
+`
+
+
+export const PostRow = styled.div`
+    border: 1px solid #EAEAEA;
+    border-radius: 15px;
+    margin : 12px;
+    padding : 13px;
+
+    img {
+        width: 29px;
+        height: 29px;
+        margin-right: 20px;
+    }
+
+    .BP_kind {
+        font-size : 13px;
+        font-weight : bold;
+        color : #0085FF;
+        padding-bottom : 13px;
+        line-height: 15px;
+    }
+
+    .BP_title {
+        font-size : 15px;
+        font-weight : bold;
+        color : #2F2F2F;
+        padding-bottom : 6px;
+        line-height: 17px;
+    }
+
+    .BP_content {
+        font-size : 15px;
+        color : #525252;
+        line-height: 17px;
+        padding-bottom : 6px;
     }
 `
