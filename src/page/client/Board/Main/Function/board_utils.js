8ff3a63720a759d6bf1871a2ac075a53cb674cea
hdev1004 <h.dev@kakao.com>
2023-07-09 15:22:34 +0900

    FIX: SERVER URL 변경

--- src/page/client/Board/Main/Function/board_utils.js
+++ src/page/client/Board/Main/Function/board_utils.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const getBoardKindList = (setData) => {
-    axios.get(process.env.REACT_APP_TEST_URL + "/board/list").then((res) => {
+    axios.get(process.env.REACT_APP_SERVER_URL + "/board/list").then((res) => {
         let data = res.data.data;
         setData(data);
     }).catch((err) => {
